beb47137a64f7b35c8ac3838661f5609acf4f0c3
Chia-Hung Lin <qaz741012@gmail.com>
2019-03-25 15:45:54 +0800

    Add ItemListElement

--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ImgDisplay from '@/components/ImgDisplay'
+import ItemListElement from '@/components/ItemListElement'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/ImgDisplay',
-      name: 'ImgDisplay',
-      component: ImgDisplay
+      path: '/ItemListElement',
+      name: 'ItemListElement',
+      component: ItemListElement
     }
   ]
 })
