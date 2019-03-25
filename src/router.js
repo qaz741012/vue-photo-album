5cfe84e83902d7d102704de81874610b5da81e88
Chia-Hung Lin <qaz741012@gmail.com>
2019-03-25 17:49:29 +0800

    Add Index

--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import PhotoForm from '@/components/PhotoForm'
+import Index from '@/views/Index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/PhotoForm',
-      name: 'PhotoForm',
-      component: PhotoForm
+      path: '/Index',
+      name: 'Index',
+      component: Index
     }
   ]
 })
