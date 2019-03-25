61926e4d305f9ec3c5a66b239666ce61cb118023
Chia-Hung Lin <qaz741012@gmail.com>
2019-03-25 16:32:14 +0800

    Add PhotoForm

--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import AuthPanel from '@/components/AuthPanel'
+import PhotoForm from '@/components/PhotoForm'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/AuthPanel',
-      name: 'AuthPanel',
-      component: AuthPanel
+      path: '/PhotoForm',
+      name: 'PhotoForm',
+      component: PhotoForm
     }
   ]
 })
