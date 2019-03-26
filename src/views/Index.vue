bd54d644f9d54cdb15394f5c116480dc42b491c5
Chia-Hung Lin <qaz741012@gmail.com>
2019-03-26 23:46:13 +0800

    Connect index api

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,23 +1,63 @@
 <template>
 <div class="container">
   <div class="item-list" v-for="photo in photos">
-    <ItemListElement/>
+    <ItemListElement :isLogin="isLogin"
+                     :id="photo.id"
+                     :title="photo.title"
+                     :description="photo.description"
+                     :url="'http://35.201.135.120/photo_album'+photo.file_location.url" />
   </div>
 </div>
 </template>
 
 <script>
 import ItemListElement from "@/components/ItemListElement"
+import axios from "axios";
 
 export default {
   data() {
     return {
       isLogin: false,
-      photos: [1,2,3,4,5]
+      photos: []
     };
   },
   components: {
     ItemListElement: ItemListElement
+  },
+  methods: {
+    handleAuthState(payload) {
+      let action = payload.action;
+      if (action === "login") {
+        this.isLogin = true;
+      } else if (action === "logout") {
+        this. isLogin = false;
+      }
+    }
+  },
+  created() {
+    this.$bus.$on("auth-state", this.handleAuthState);
+
+    let sessionData = JSON.parse(localStorage.getItem("photo-album-user"));
+    if (sessionData) {
+      this.handleAuthState({ action: "login" });
+    } else {
+      this.handleAuthState({ action: "logout" });
+    }
+
+    // photos api
+    let url = "http://35.201.135.120/photo_album/api/v1/photos";
+    let that = this;
+    axios
+      .get(url, {})
+      .then(function(res) {
+        that.photos = res.data.data;
+      })
+      .catch(function(err) {
+        console.error(err.response.data);
+      });
+  },
+  beforeDestroy() {
+    this.$bus.$off("auth-state", this.handleAuthState);
   }
 }
 </script>
