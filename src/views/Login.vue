c14c2a28b43cfbdb0e8f2335f73fc428452ade26
Chia-Hung Lin <qaz741012@gmail.com>
2019-03-26 18:06:52 +0800

    Connect login api

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,43 @@
 <template>
 <div class="login">
-  <AuthPanel type="login" />
+  <AuthPanel type="login" @auth-panel-submit="handleLogin" />
 </div>
 </template>
 
 <script>
 import AuthPanel from "@/components/AuthPanel";
+import axios from "axios";
 
 export default {
   components: {
     AuthPanel: AuthPanel
+  },
+  methods: {
+    handleLogin(payload) {
+      console.log("handleLogin");
+      var that = this;
+      var url = "http://35.201.135.120/photo_album/api/v1/login";
+      axios
+        .post(url, payload)
+        .then(function(res) {
+          console.log("login success");
+          console.log(res.data);
+
+          //save auth_token to local storage
+          var authToken = res.data.auth_token;
+          var email = payload.email;
+          var sessionData = { authToken: authToken, email: email };
+          localStorage.setItem("photo-album-user", JSON.stringify(sessionData));
+
+          //emit auth-state to $bus
+          that.$bus.$emit("auth-state", { action: "login" })
+
+          that.$router.push("/");
+        })
+        .catch(function(err) {
+          console.error(err.response.data.message);
+        });
+    }
   }
 }
 </script>
